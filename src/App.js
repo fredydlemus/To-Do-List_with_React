1d4d9a90beb98d60d481ac9cb4d15bbc525a5464
fredyflemus <fredyflemus@gmail.com>
2021-10-02 16:27:50 -0600

    function complete and delete toDo's

--- src/App.js
+++ src/App.js
@@ -40,6 +40,21 @@ function App() {
     })
   }
 
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  }
+
+
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex, 1)
+    setTodos(newTodos);
+  }
+
   return (
     <React.Fragment>
       <TodoHeader>
@@ -61,7 +76,10 @@ function App() {
             <TodoItem 
               key={todo.text} 
               text={todo.text}
-              completed={todo.completed} />
+              completed={todo.completed}
+              onComplete={() => completeTodo(todo.text)}
+              onDelete={() => deleteTodo(todo.text)}
+              />
           ))} 
         </TodoList>
       </TodoDay>
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,15 +2,7 @@ import React from "react";
 
 function TodoItem(props){
 
-    const onCompleted = () =>{
-        alert('Completado todo' + props.text);
-    }
-
-    const onDeleted = () =>{
-        alert('Borraste todo' + props.text);
-    }
-
-
+    
     return (
         <li className={`item ${props.completed && 'item--completed'}`}>
             <div className={`item-text-wrapper ${props.completed && 'item-text-wrapper--completed'}`}>
@@ -19,13 +11,13 @@ function TodoItem(props){
             <div className={`item-buttons-wrapper ${props.completed && 'item-buttons-wrapper--completed'}`}>
                 <p className={`item-buttons-wrapper__button-content ${props.completed && 'item-buttons-wrapper__button-content--completed'}`}>Delete</p>
                 <span 
-                    className={`item-buttons-wrapper__button ${props.completed && 'item-buttons-wrapper__button--deleted'}`}
-                    onClick={onDeleted}
+                    className={`item-buttons-wrapper__button item-buttons-wrapper__button--delete ${props.completed && 'item-buttons-wrapper__button--deleted'}`}
+                    onClick={props.onDelete}
                     >X</span>
                 <p className={`item-buttons-wrapper__button-content ${props.completed && 'item-buttons-wrapper__button-content--completed'}`}>Complete</p>
                 <span 
-                    className={`item-buttons-wrapper__button ${props.completed && 'item-buttons-wrapper__button--completed'}`}
-                    onClick={onCompleted}
+                    className={`item-buttons-wrapper__button item-buttons-wrapper__button--complete ${props.completed && 'item-buttons-wrapper__button--completed'}`}
+                    onClick={props.onComplete}
                     >ᄼ</span>
             </div>
             
